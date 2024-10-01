a502924cc468d3dd0fc2b22fb08039a959ca6fd6
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2024-10-01 11:18:05 -0400

    Add more objects/collision

--- TopDownGame/Assets/Scripts/PlayerController.cs
+++ TopDownGame/Assets/Scripts/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
 
     //sprite variables
     public Sprite upSprite;
-    public Sprite downSprite;
+    public Sprite frontSprite;
     public Sprite rightSprite;
     public Sprite leftSprite;
     
@@ -28,25 +28,45 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey("d")) 
         {
+            //go right
             newPosition.x += .01f;
+            //change sprite to right sprite
+            //sr.sprite = rightSprite;
         }
 
         if (Input.GetKey("a"))
         {
+            //go left
             newPosition.x -= .01f;
+            //change sprite to left sprite
+            //sr.sprite = leftSprite;
         }
 
         if (Input.GetKey("w"))
         {
+            //go up
             newPosition.y += .01f;
+            //change sprite to up sprite
+            //sr.sprite = upSprite;
         }
 
         if (Input.GetKey("s"))
         {
+            //go down
             newPosition.y -= .01f;
+            //sr.sprite = frontSprite;
         }
 
         transform.position = newPosition;
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if colliding with a game object with specific tag
+        if(collision.gameObject.tag.Equals("Door1"))
+        {
+            Debug.Log("change scene");
+        }
     }
 }
